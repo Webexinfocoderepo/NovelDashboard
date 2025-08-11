8e0f82f29c612d9ded5da0969c20b93fc536ad30
akshay <9927akshay@gmail.com>
2025-08-11 20:03:50 +0530

    changes

--- Nodejs/routes/visitor.js
+++ Nodejs/routes/visitor.js
@@ -25,16 +25,16 @@ router.get('/track', async (req, res) => {
     // Redirect based on action
     switch(action) {
         case 'Complete':
-            res.redirect('http://13.235.83.129/api/complete');
+            res.redirect('http://www.thenovelresearch.com/api/complete');
             break;
         case 'Terminate':
-            res.redirect('http://13.235.83.129/api/terminate');
+            res.redirect('http://www.thenovelresearch.com/api/terminate');
             break;
         case 'Quotafull':
-            res.redirect('http://13.235.83.129/api/quotafull');
+            res.redirect('http://www.thenovelresearch.com/api/quotafull');
             break;
         default:
-            res.redirect('http://13.235.83.129/api/terminate'); // Default to terminate page
+            res.redirect('http://www.thenovelresearch.com/api/terminate'); // Default to terminate page
     }
   } catch (err) {
     res.status(500).send("Something went wrong.");
